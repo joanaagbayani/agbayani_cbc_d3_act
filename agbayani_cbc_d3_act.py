54acf111796c8019ea9117cd35ae982c84ebb57f
joanaagbayani <joana.hasel.agbayani@aclcbutuan.edu.ph>
2025-02-18 20:21:10 +0800

    Add files via upload

---
+++ agbayani_cbc_d3_act.py
@@ -0,0 +1,9 @@
+strings = "Hello Everyone"
+
+print(f"Testing: {strings}")
+print(f"Printable: {strings.isprintable()}")
+print(f"Identifier: {strings.isidentifier()}")
+print(f"Title case:: {strings.istitle()}")
+print(f"Uppercase: {strings.isupper()}")
+print(f"Numeric: {strings.isnumeric()}")
+print("-" * 20)
